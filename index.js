e5d86292975180758ee9966e65b7e7ff0e76f384
Erin Green <erin.y.green@gmail.com>
2018-07-16 19:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ function theBeatlesPlay(musicians, instruments){
   return newArray;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(facts) {
   var newArray2 = []
   while (facts) {
-    c
+    newArray2.push(facts[i] + "!!!")
   }
   return newArray2;
 }
